03df2d5eecc3cb175e964e6535d27c19e36268a3
kub <derkub@gmail.com>
2018-04-24 21:52:37 +0200

    kludge for irix 6.5.16+

--- linux-user/syscall.c
+++ linux-user/syscall.c
@@ -15898,6 +15898,7 @@ static abi_long do_syscall1(void *cpu_env, int num, abi_long arg1,
 	case TARGET_NR_syssgi_rldenv:
 	case TARGET_NR_syssgi_tosstsave:
 	case TARGET_NR_syssgi_fpbcopy:
+	case TARGET_NR_syssgi_getprocattr: /* ? 2nd=string 3rd=result ptr */
 	    ret = 0;
 	    break;
         case TARGET_NR_syssgi_setgroups:
